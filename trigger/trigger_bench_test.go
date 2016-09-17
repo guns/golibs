6f00d858fb7768ec49e7e15a64e3e33d3da2f5b8
guns <self@sungpae.com>
2016-09-16 21:52:10 -0500

    Move benchmarks to own file

---
+++ trigger/trigger_bench_test.go
@@ -0,0 +1,53 @@
+package trigger
+
+import (
+	"sync"
+	"testing"
+)
+
+//
+// BenchmarkTrigger-4    3000000  578 ns/op  224 B/op  4 allocs/op
+// BenchmarkChan-4       3000000  502 ns/op  192 B/op  2 allocs/op
+// BenchmarkWaitGroup-4  3000000  500 ns/op  32  B/op  2 allocs/op
+//
+
+func BenchmarkTrigger(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		trg := New()
+		reply := New()
+		go func() {
+			trg.Wait()
+			reply.Trigger()
+		}()
+		trg.Trigger()
+		reply.Wait()
+	}
+}
+
+func BenchmarkChan(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		trg := make(chan struct{})
+		reply := make(chan struct{})
+		go func() {
+			<-trg
+			close(reply)
+		}()
+		close(trg)
+		<-reply
+	}
+}
+
+func BenchmarkWaitGroup(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		trg := sync.WaitGroup{}
+		trg.Add(1)
+		reply := sync.WaitGroup{}
+		reply.Add(1)
+		go func() {
+			trg.Wait()
+			reply.Done()
+		}()
+		trg.Done()
+		reply.Wait()
+	}
+}
--- trigger/trigger_test.go
+++ trigger/trigger_test.go
@@ -1,9 +1,6 @@
 package trigger
 
-import (
-	"sync"
-	"testing"
-)
+import "testing"
 
 func TestTrigger(t *testing.T) {
 	exit := New()
@@ -45,52 +42,5 @@ func TestMake(t *testing.T) {
 
 	if !trg.u.Activated() {
 		t.Errorf("expected: trg.u.Activated()")
-	}
-}
-
-//
-// BenchmarkTrigger-4    3000000  578 ns/op  224 B/op  4 allocs/op
-// BenchmarkChan-4       3000000  502 ns/op  192 B/op  2 allocs/op
-// BenchmarkWaitGroup-4  3000000  500 ns/op  32  B/op  2 allocs/op
-//
-
-func BenchmarkTrigger(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		trg := New()
-		reply := New()
-		go func() {
-			trg.Wait()
-			reply.Trigger()
-		}()
-		trg.Trigger()
-		reply.Wait()
-	}
-}
-
-func BenchmarkChan(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		trg := make(chan struct{})
-		reply := make(chan struct{})
-		go func() {
-			<-trg
-			close(reply)
-		}()
-		close(trg)
-		<-reply
-	}
-}
-
-func BenchmarkWaitGroup(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		trg := sync.WaitGroup{}
-		trg.Add(1)
-		reply := sync.WaitGroup{}
-		reply.Add(1)
-		go func() {
-			trg.Wait()
-			reply.Done()
-		}()
-		trg.Done()
-		reply.Wait()
 	}
 }
