9702a3ed587398ff6dce80fc5f5d3f6c34b33960
guns <self@sungpae.com>
2017-09-07 22:30:21 -0500

    Replace editpipe with editreader

---
+++ editreader/editreader.go
@@ -0,0 +1,300 @@
+// Copyright (c) 2017 Sung Pae <self@sungpae.com>
+// Distributed under the MIT license.
+// http://www.opensource.org/licenses/mit-license.php
+
+// Package editreader provides a buffered reader that enables basic editing.
+package editreader
+
+import (
+	"io"
+	"unicode"
+)
+
+// An Op communicates an edit operation.
+type Op byte
+
+// When multiple Ops are combined, each action will be executed in
+// least-significant-order.
+//
+//	Erase | Append | Flush | Close
+//
+// For example, the value above instructs an editreader to erase the last byte
+// in the buffer, append the current byte, flush the buffer, and finally close.
+const (
+	Erase     Op = 1 << iota // Clear and remove one byte at end of buffer
+	EraseWord                // Clear and remove non-whitespace sequence at end of buffer
+	Kill                     // Truncate entire buffer
+	Append                   // Append to buffer
+	Flush                    // Flush the buffer to readers
+	Close                    // Close this editreader and send EOF to future readers
+)
+
+// An EditFn specifies the semantics of an editreader. i is the index of the
+// buffer where b would be appended.
+type EditFn func(i int, b byte) Op
+
+// T wraps a Reader and implements an editable buffer by reading one byte at a
+// time from the wrapped Reader and executing the Ops returned by an EditFn.
+type T struct {
+	f         EditFn
+	src       io.Reader // Source
+	rbuf      []byte    // 1-byte buffer for reading from r
+	buf       []byte    // Edit buffer
+	r         int       // Next edit buffer read index
+	w         int       // Next edit buffer write index
+	rerr      error     // Error to return readers after close
+	available bool      // Set on Flush, cleared after full read
+	overflow  bool      // Set when an Append would overflow the edit buffer
+	done      bool      // Set on read error/EOF or Close Op
+	secure    bool      // Clear edit buffer when appropriate
+}
+
+// New returns an editreader with an buffer buflen bytes long. If f is nil,
+// BasicLineEdit is used as the edit function. If buflen is non-positive, a
+// default buflen of 4096 is used.
+//
+// Unflushed input that exceeds buflen is flushed automatically.
+//
+// If secure is true, the buffer is cleared on Kill, Close, and after reads.
+// Note that input that is flushed but unread is not cleared on Close. Since
+// the data will be cleared on read, callers should make sure to drain the
+// editreader if they care about zeroing buffers. Erase and EraseWord always
+// zero the bytes they erase.
+func New(r io.Reader, buflen int, secure bool, f EditFn) *T {
+	if buflen < 1 {
+		buflen = 4096
+	}
+	if f == nil {
+		f = BasicLineEdit
+	}
+	return &T{
+		f:      f,
+		src:    r,
+		rbuf:   make([]byte, 1),
+		buf:    make([]byte, buflen),
+		secure: secure,
+	}
+}
+
+// Read reads from the edit buffer if it is available for read. If there is no
+// data available, data is read and processed from the source reader until it
+// is flushed and available for read.
+func (e *T) Read(dst []byte) (n int, err error) {
+	if e.available {
+		return e.readAvailable(dst)
+	}
+
+	if e.done {
+		return 0, e.rerr
+	}
+
+	for {
+		e.scan()
+		if e.available {
+			break
+		} else if e.done {
+			return 0, e.rerr
+		}
+	}
+
+	return e.readAvailable(dst)
+}
+
+// WriteTo implements WriterTo. This is explicitly to avoid use of a transfer
+// buffer in io.Copy, which is called by exec.Cmd to pipe data from a non-file
+// stdin.
+func (e *T) WriteTo(w io.Writer) (n int64, err error) {
+	var i, j int
+	var rerr, werr error
+	buf := make([]byte, 4096)
+	for {
+		i, rerr = e.Read(buf)
+		if i > 0 {
+			j, werr = w.Write(buf[:i])
+			n += int64(j)
+			if werr != nil {
+				err = werr
+				break
+			}
+		}
+		if rerr != nil {
+			if rerr != io.EOF {
+				err = rerr
+			}
+			break
+		}
+	}
+	return n, err
+}
+
+// readAvailable copies unread data into dst. The available flag is cleared
+// if no unread data remains.
+// WARNING: This method assumes the buffer is available for read!
+func (e *T) readAvailable(dst []byte) (n int, err error) {
+	n = copy(dst, e.buf[e.r:e.w])
+	i := e.r + n
+	if e.secure {
+		clearbytes(e.buf[e.r:i])
+	}
+	e.r = i
+	if e.r >= e.w {
+		e.r = 0
+		e.w = 0
+		e.available = false
+	}
+	return n, nil
+}
+
+// scan reads and processes one byte from the source reader. If there was an
+// overflow from the last scan, no read occurs and the previously read byte is
+// processed instead.
+func (e *T) scan() {
+	if e.overflow {
+		e.overflow = false
+		e.process(e.rbuf[0])
+		return
+	}
+
+	n, err := e.src.Read(e.rbuf)
+	if n > 0 {
+		e.process(e.rbuf[0])
+	}
+	if err != nil {
+		e.closeWithError(err)
+	}
+}
+
+// process executes the Ops specified by the EditFn for byte b.
+func (e *T) process(b byte) {
+	ops := e.f(e.w, b)
+
+	if ops&Erase > 0 {
+		e.erase()
+	}
+	if ops&EraseWord > 0 {
+		e.eraseWord()
+	}
+	if ops&Kill > 0 {
+		e.kill()
+	}
+	if ops&Append > 0 {
+		e.append(b)
+	}
+	if ops&Flush > 0 {
+		e.available = true
+	}
+	if ops&Close > 0 {
+		e.closeWithError(nil)
+	}
+}
+
+// erase prunes and zeroes the last byte in the edit buffer.
+func (e *T) erase() {
+	if e.w <= 0 {
+		return
+	}
+	e.w--
+	e.buf[e.w] = 0
+}
+
+// eraseWord prunes the last sequence of non-whitespace bytes in the write
+// buffer. Multibyte character sequences are unsupported.
+func (e *T) eraseWord() {
+	if e.w <= 0 {
+		return
+	}
+
+	// number of boundary transitions
+	n := 2
+
+	e.w--
+	if isWordRune(rune(e.buf[e.w])) {
+		n--
+	}
+	e.buf[e.w] = 0
+
+	for e.w > 0 {
+		e.w--
+		isword := isWordRune(rune(e.buf[e.w]))
+		if n == 2 && isword {
+			n--
+		} else if n == 1 && !isword {
+			e.w++
+			break
+		}
+		e.buf[e.w] = 0
+	}
+}
+
+// kill truncates the edit buffer
+func (e *T) kill() {
+	if e.secure {
+		clearbytes(e.buf[:e.w])
+	}
+	e.w = 0
+}
+
+// append appends a byte to the edit buffer. If this would overflow the
+// buffer, the overflow flag is set and the buffer is marked as available.
+func (e *T) append(b byte) {
+	if e.w >= len(e.buf) {
+		e.overflow = true
+		e.available = true
+		return
+	}
+	e.buf[e.w] = b
+	e.w++
+}
+
+// closeWithError marks e as done, and sets err as the error to send to readers. If err
+// is nil, io.EOF is sent.
+func (e *T) closeWithError(err error) {
+	if e.done {
+		return
+	}
+	e.done = true
+	e.rerr = err
+	if err == nil {
+		e.rerr = io.EOF
+	}
+	if e.secure {
+		e.rbuf[0] = 0
+		n := e.w
+		if e.available {
+			n = e.r
+		}
+		clearbytes(e.buf[:n])
+	}
+}
+
+// BasicLineEdit specifies a simple line editor.
+func BasicLineEdit(i int, b byte) Op {
+	switch b {
+	case '\b', 0x7f: // ^H, ^?
+		return Erase
+	case 0x17: // ^W
+		return EraseWord
+	case 0x15: // ^U
+		return Kill
+	case 0x04: // ^D
+		if i == 0 {
+			return Close
+		}
+		return Flush
+	case '\n':
+		return Append | Flush
+	default:
+		return Append
+	}
+}
+
+// clearbytes zeros a byte slice.
+func clearbytes(bs []byte) {
+	for i := range bs {
+		bs[i] = 0
+	}
+}
+
+func isWordRune(r rune) bool {
+	return r != ' ' && unicode.IsPrint(r)
+}
---
+++ editreader/editreader_test.go
@@ -0,0 +1,163 @@
+// Copyright (c) 2017 Sung Pae <self@sungpae.com>
+// Distributed under the MIT license.
+// http://www.opensource.org/licenses/mit-license.php
+
+package editreader
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+	"unicode"
+)
+
+func TestEditReader(t *testing.T) {
+	data := []struct {
+		input  string
+		rlen   int
+		wlen   int
+		editFn EditFn
+		expect string
+	}{
+		// Simple inputs
+		{input: "\n", expect: "\n"},
+		{input: "foo bar baz\n", expect: "foo bar baz\n"},
+		{input: "lorem ipsum dolor\n\nsit amet\n", expect: "lorem ipsum dolor\n\nsit amet\n"},
+		{input: "\x00\x01\n\x02\n", expect: "\x00\x01\n\x02\n"},
+
+		// Unflushed inputs
+		{input: "", expect: ""},
+		{input: "foo bar baz", expect: ""},
+		{input: "foo\nbar baz", expect: "foo\n"},
+
+		// Partial reads
+		{input: "1234567890\n1234567890\n", wlen: 3, expect: "1234567890\n1234567890\n"},
+
+		// Overflow
+		{input: "1234567890\n", rlen: 3, expect: "1234567890\n"},
+		{input: "1234567890", rlen: 3, expect: "123456789"},
+
+		// Erase
+		{input: "\b\n", expect: "\n"},
+		{input: "12\x7f\b\x7f345\n", expect: "345\n"},
+		{input: "1234567\b\x7f\b890\n", expect: "1234890\n"},
+
+		// EraseWord
+		{input: "\x17\n", expect: "\n"},
+		{input: " \x17\n", expect: "\n"},
+		{input: "foo\x17\n", expect: "\n"},
+		{input: "foo \t\x17\n", expect: "\n"},
+		{input: "foo bar\x17\n", expect: "foo \n"},
+		{input: "foo bar \x17\n", expect: "foo \n"},
+		{input: "foo bar\x00\x01\x02\x17\n", expect: "foo \n"},
+
+		// Kill
+		{input: "\x15\n", expect: "\n"},
+		{input: "foo bar baz\x15\n", expect: "\n"},
+		{input: "foo\nbar \x15baz\n", expect: "foo\nbaz\n"},
+
+		// Close
+		{input: "\x04", expect: ""},
+		{input: "foo\x04", expect: "foo"},
+		{input: "foo\x04\x04bar\n", expect: "foo"},
+
+		// Custom EditFn
+		{
+			input: "one 2 three 4 five\n",
+			editFn: func(i int, b byte) Op {
+				if unicode.IsNumber(rune(b)) {
+					return 0
+				}
+				return BasicLineEdit(i, b)
+			},
+			expect: "one  three  five\n",
+		},
+		{
+			// This case tests handlind of unread data after close
+			input: "foo\b\x7f\x17\x15\x04bar\n\x07baz\n\x07",
+			editFn: func(i int, b byte) Op {
+				if b == 0x07 {
+					return Flush | Close
+				}
+				return Append
+			},
+			expect: "foo\b\x7f\x17\x15\x04bar\n",
+		},
+	}
+
+	for _, row := range data {
+		for _, writeTo := range []bool{false, true} {
+			if row.wlen == 0 {
+				row.wlen = 80
+			}
+
+			r := New(strings.NewReader(row.input), row.rlen, true, row.editFn)
+			out := make([]byte, 0, 256)
+			buf := make([]byte, row.wlen)
+			var n int
+			var err error
+
+			out, buf, err = readall(r, out, buf, writeTo)
+			if err != io.EOF {
+				t.Errorf("unexpected non-EOF error: %#v", err)
+			}
+
+			// Read once more; should get (0, EOF)
+			n, err = r.Read(buf)
+			if n != 0 {
+				t.Errorf("%#v != %#v", n, 0)
+			}
+			if err != io.EOF {
+				t.Errorf("%#v != %#v", err, io.EOF)
+			}
+
+			if len(r.rbuf) != 1 {
+				t.Errorf("%#v != %#v", len(r.rbuf), 1)
+			}
+
+			if r.rbuf[0] != 0 {
+				t.Errorf("should be clear: %#v", r.rbuf[0])
+			}
+
+			for i := range r.buf {
+				if r.buf[i] != 0 {
+					t.Errorf("should be clear: %#v", string(r.buf))
+					break
+				}
+			}
+
+			if string(out) != row.expect {
+				t.Errorf("%#v != %#v", string(out), row.expect)
+			}
+		}
+	}
+}
+
+func readall(r *T, out, buf []byte, writeTo bool) ([]byte, []byte, error) {
+	var n int
+	var n64 int64
+	var err error
+
+	if writeTo {
+		bbuf := bytes.Buffer{}
+		n64, err = r.WriteTo(&bbuf)
+		n = int(n64)
+		out = append(out, bbuf.Bytes()...)
+		if err == nil {
+			err = io.EOF
+		}
+	} else {
+		for {
+			n, err = r.Read(buf)
+			if n > 0 {
+				out = append(out, buf[:n]...)
+			}
+			if err != nil {
+				break
+			}
+		}
+	}
+
+	return out, buf, err
+}
