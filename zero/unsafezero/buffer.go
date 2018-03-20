45d772f54309b81d69c88e912037534e991d6293
guns <self@sungpae.com>
2018-03-20 16:24:24 -0500

    Add 1.10 compat for ClearBuffer

--- zero/unsafezero/buffer.go
+++ zero/unsafezero/buffer.go
@@ -19,23 +19,26 @@ const (
 	go1_0
 	go1_8
 	go1_9
+	go1_10
 )
 
 var goBufferVersion = goVersionUnknown
 
 func init() {
 	v := runtime.Version()
-	if len(v) < 5 {
+
+	switch {
+	case len(v) < 5:
 		if v == "go1" {
 			goBufferVersion = go1_0
 		}
 		return
-	}
-	switch v[:5] {
-	case "go1.8":
+	case v[:5] == "go1.8":
 		goBufferVersion = go1_8
-	case "go1.9":
+	case v[:5] == "go1.9":
 		goBufferVersion = go1_9
+	case v[:6] == "go1.10":
+		goBufferVersion = go1_10
 	}
 }
 
@@ -61,9 +64,25 @@ type go1_9BytesBuffer struct {
 	bootstrap [64]byte // memory to hold first slice; helps small buffers avoid allocation.
 }
 
+type go1_10BytesBuffer struct {
+	buf       []byte   // contents are the bytes buf[off : len(buf)]
+	off       int      // read at &buf[off], write at &buf[len(buf)]
+	bootstrap [64]byte // memory to hold first slice; helps small buffers avoid allocation.
+	lastRead  int8     // last read operation, so that Unread* can work correctly.
+}
+
 // ClearBuffer zeroes ALL data in a bytes.Buffer
 func ClearBuffer(bbuf *bytes.Buffer) {
 	switch goBufferVersion {
+	case go1_10:
+		b := (*go1_10BytesBuffer)(unsafe.Pointer(bbuf))
+		zero.ClearBytes(b.buf)
+		b.buf = b.buf[:0]
+		b.off = 0
+		for i := range b.bootstrap {
+			b.bootstrap[i] = 0
+		}
+		b.lastRead = 0
 	case go1_9:
 		b := (*go1_9BytesBuffer)(unsafe.Pointer(bbuf))
 		zero.ClearBytes(b.buf)
