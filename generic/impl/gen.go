974deadd5ccf77d5ccf0475fc6b7cee1b40cad11
guns <self@sungpae.com>
2018-07-17 13:39:57 -0500

    Add pkg generic/impl for ints

---
+++ generic/impl/gen.go
@@ -0,0 +1,7 @@
+// Package impl provides common concrete implementations of generic templates
+// from github.com/guns/golibs/generic
+package impl
+
+//go:generate genny -pkg=impl -in=../queue.go -out=queue.go gen GenericType=int
+//go:generate genny -pkg=impl -in=../stack.go -out=stack.go gen GenericType=int
+//go:generate genny -pkg=impl -in=../quicksort.go -out=quicksort.go gen GenericNumber=int
