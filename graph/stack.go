98de9015b2d2fd26c5bcb0cf28480a736e7411ce
guns <self@sungpae.com>
2018-07-24 02:35:55 -0500

    Add autoPromotingStack/nonPromotingStack

---
+++ graph/stack.go
@@ -0,0 +1,139 @@
+// Copyright (c) 2018 Sung Pae <self@sungpae.com>
+// Distributed under the MIT license.
+// http://www.opensource.org/licenses/mit-license.php
+
+package graph
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+type listNode struct {
+	prev, next int
+}
+
+func (node listNode) undefined() bool {
+	return node.prev == undefined && node.next == undefined
+}
+
+func makeListNodeSlice(buf []int) []listNode {
+	s := (*[]listNode)(unsafe.Pointer(&buf))
+
+	// listNode is two ints long
+	header := (*reflect.SliceHeader)(unsafe.Pointer(s))
+	header.Len = len(buf) / 2
+	header.Cap = cap(buf) / 2
+
+	return *s
+}
+
+// autoPromotingStack and nonPromotingStack implement the memory-sharing dual
+// stack data structure described by Tim Leslie at http://www.timl.id.au/SCC.
+//
+// autoPromotingStack is a stack with the following interesting property:
+// pushing an int that is already on the stack promotes that int to the top.
+// This can be done in constant time with a slice-backed doubly linked list
+// whose node values are the indices of the slice.
+//
+// nonPromotingStack is a simple stack that can share memory with an
+// autoPromotingStack provided the following constraints hold:
+//
+//	1) A nonPromotingStack never contains duplicates.
+//	2) The sets of elements in an autoPromotingStack and nonPromotingStack
+//	   are always disjoint.
+
+type autoPromotingStack struct {
+	s   []listNode
+	top int
+	len int
+}
+
+func newAutoPromotingStack(buf []int) *autoPromotingStack {
+	return &autoPromotingStack{
+		s:   makeListNodeSlice(buf),
+		top: undefined,
+		len: 0,
+	}
+}
+
+func (aps *autoPromotingStack) peek() int {
+	return aps.top
+}
+
+func (aps *autoPromotingStack) pop() int {
+	oldtop := aps.top
+	aps.top = aps.s[oldtop].prev
+
+	aps.s[oldtop].prev = undefined
+	if aps.top != undefined {
+		aps.s[aps.top].next = undefined
+	}
+	aps.len--
+
+	return oldtop
+}
+
+// If index n is on the stack, it is promoted to be the top element.
+func (aps *autoPromotingStack) push(n int) {
+	if aps.s[n].undefined() {
+		// Standard doubly linked list append
+		if aps.top != undefined {
+			aps.s[aps.top].next = n
+		}
+		aps.s[n].prev = aps.top
+		aps.top = n
+		aps.len++
+		return
+	} else if n == aps.top {
+		// n is already promoted
+		return
+	}
+
+	node := aps.s[n]
+
+	// Unlink node and splice
+	if node.prev != undefined {
+		aps.s[node.prev].next = node.next
+	}
+	aps.s[node.next].prev = node.prev
+
+	// Promote node
+	if aps.top != undefined {
+		aps.s[aps.top].next = n
+	}
+	aps.s[n].prev = aps.top
+	aps.s[n].next = undefined
+	aps.top = n
+}
+
+type nonPromotingStack struct {
+	s   []listNode
+	top int
+	len int
+}
+
+func newNonPromotingStack(buf []int) *nonPromotingStack {
+	return &nonPromotingStack{
+		s:   makeListNodeSlice(buf),
+		top: undefined,
+		len: 0,
+	}
+}
+
+func (nps *nonPromotingStack) peek() int {
+	return nps.top
+}
+
+func (nps *nonPromotingStack) pop() int {
+	top := nps.top
+	nps.top = nps.s[top].prev
+	nps.len--
+	return top
+}
+
+func (nps *nonPromotingStack) push(n int) {
+	nps.s[n].prev = nps.top
+	nps.top = n
+	nps.len++
+}
---
+++ graph/stack_test.go
@@ -0,0 +1,116 @@
+// Copyright (c) 2018 Sung Pae <self@sungpae.com>
+// Distributed under the MIT license.
+// http://www.opensource.org/licenses/mit-license.php
+
+package graph
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAutoPromotingStack(t *testing.T) {
+	const peek = -1
+	const pop = -2
+	const peekNPS = -3
+	const popNPS = -4
+	const transfer = -5
+
+	data := []struct {
+		len  int
+		cmds []int
+		out  []int
+	}{
+		{
+			len:  2,
+			cmds: []int{0, 1},
+			out:  []int{0, 1},
+		},
+		{
+			len:  2,
+			cmds: []int{0, 1, pop},
+			out:  []int{1, 0},
+		},
+		{
+			len:  2,
+			cmds: []int{0, 1, peek},
+			out:  []int{1, 0, 1},
+		},
+		{
+			len:  4,
+			cmds: []int{0, 1, 2, 1, 3, 3},
+			out:  []int{0, 2, 1, 3},
+		},
+		{
+			len:  4,
+			cmds: []int{3, 2, 1, transfer, peekNPS, 0, transfer, popNPS},
+			out:  []int{1, 0, 3, 2, 1},
+		},
+		{
+			len:  4,
+			cmds: []int{0, 1, 2, 3, 0, 1, 2, 3, 2, 1, 0, 0, 1, 2, 3},
+			out:  []int{0, 1, 2, 3},
+		},
+	}
+
+	for _, row := range data {
+		buf := make([]int, row.len*2)
+		aps := newAutoPromotingStack(buf)
+		nps := newNonPromotingStack(buf)
+
+		for i := range buf {
+			buf[i] = -1
+		}
+
+		if len(aps.s) != len(buf)/2 {
+			t.Errorf("%v != %v", len(aps.s), len(buf)/2)
+		}
+
+		out := make([]int, 0, len(row.cmds))
+
+		for _, n := range row.cmds {
+			switch n {
+			case peek:
+				out = append(out, aps.peek())
+			case pop:
+				out = append(out, aps.pop())
+			case peekNPS:
+				out = append(out, nps.peek())
+			case popNPS:
+				out = append(out, nps.pop())
+			case transfer:
+				nps.push(aps.pop())
+			default:
+				aps.push(n)
+			}
+		}
+
+		for aps.len > 0 {
+			nps.push(aps.pop())
+		}
+
+		for nps.len > 0 {
+			out = append(out, nps.pop())
+		}
+
+		if !reflect.DeepEqual(out, row.out) {
+			t.Errorf("%v != %v", out, row.out)
+		}
+
+		for i := range buf {
+			buf[i] = 0xff
+		}
+
+		for i := range aps.s {
+			if aps.s[i].prev != 0xff || aps.s[i].next != 0xff ||
+				nps.s[i].prev != 0xff || nps.s[i].next != 0xff {
+				t.Log("All values should be 0xff")
+				t.Logf("buf: %v", buf)
+				t.Logf("aps: %v", aps)
+				t.Logf("nps: %v", nps)
+				t.Fail()
+				break
+			}
+		}
+	}
+}
