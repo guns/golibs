8a8577ce6c8dcdcf5778f59f841ed6db1ae1d857
guns <self@sungpae.com>
2018-07-25 16:36:15 -0500

    Add Pearce's strongly connected components algorithm
    See graph/scc.txt for details on the algorithm.
    
    The space complexity of Tarjan's then 33-years old SCC algorithm was
    improved in 2005 by David Pearce. [1] The worst case space complexity of
    the algorithm was reduced from O(v(2 + 5w)) to O(3vw) by eliminating the
    visit-index table and interleaving the DFS and backtracking stacks into
    a single O(vw) data structure.
    
    Tim Leslie added an iterative implementation of Pearce's algorithm
    to SciPy in 2013. [2] Instead of using continuations to convert the
    recursive calls, Leslie eliminated the need for any continuations by
    creating a stack that promotes existing items in the stack to the top in
    constant time. This data structure takes O(2vw) space, but eliminates
    the need for a continuation variable per vertex (O(vw)), thus preserving
    Pearce's original O(3vw) worst case space complexity.
    
    Leslie's implementation does not utilize the call stack and is also very
    easy to read and implement, so it is the algorithm that is implemented
    here.
    
    Many thanks to @DavePearce and @timleslie for their work!
    
    [1]: http://homepages.ecs.vuw.ac.nz/~djp/files/IPL15-preprint.pdf
    [2]: http://www.timl.id.au/SCC

--- graph/graph.go
+++ graph/graph.go
@@ -5,7 +5,11 @@
 // Package graph provides a general-purpose directed graph implementation.
 package graph
 
-import "math/bits"
+import (
+	"math/bits"
+
+	"github.com/guns/golibs/generic/impl"
+)
 
 // A Graph is a set { (V, E) : V ⊆ ℕ⁰ and E ⊆ { (u, v) ∈ V } }.
 //
@@ -157,6 +161,99 @@ func (g Graph) Transpose(h Graph) Graph {
 	}
 
 	return h
+}
+
+// StronglyConnectedComponents returns a forest of strongly connected
+// components in reverse topological order. This forest of vertex indices is
+// written to scc, which is grown if necessary.
+//
+// Note that the returned [][]int is actually backed by a single []int
+// (scc[0][:cap(scc[0])]) to minimize allocations, and is therefore NOT fit
+// for modification.
+//
+// Passing the same [][]int returned by this function as the scc parameter
+// reuses memory and can eliminate unnecessary allocations.
+func (g Graph) StronglyConnectedComponents(scc [][]int, w *Workspace) [][]int {
+	w.prepare(len(g), wANeg)
+
+	// Tim Leslie's iterative implementation [1] of David Pearce's
+	// memory-efficient strongly connected components algorithm. [2]
+	//
+	// [1]: http://www.timl.id.au/SCC
+	// [2]: http://homepages.ecs.vuw.ac.nz/~djp/files/IPL15-preprint.pdf
+
+	rindex := w.a                                 // |V|w  · Array of v -> local root index
+	dfs, backtrack := w.makeSharedStacks(wB | wC) // 2|V|w · DFS/backtrack shared stack
+
+	builder := impl.NewPacked2DIntBuilderFromRows(scc)
+	builder.Grow(len(g) - builder.Cap())
+	builder.SetAutoGrow(false)
+
+	i := 1
+	component := len(g) - 1
+
+	for u := range g {
+		if rindex[u] != undefined {
+			continue
+		}
+
+		// DFS
+		dfs.pushOrPromote(u)
+
+		for dfs.len > 0 {
+			u := dfs.peek()
+
+			if rindex[u] == undefined {
+				// Top of dfs is unvisited, so assign it an index and push or promote its
+				// unvisited successors.
+
+				rindex[u] = i
+				i++
+
+				for _, v := range g[u] {
+					if rindex[v] == undefined {
+						dfs.pushOrPromote(v)
+					}
+				}
+			} else {
+				// Top of dfs has been visited, so compare it against successors to find a
+				// minimum local root index.
+
+				u = dfs.pop()
+				root := true
+
+				for _, v := range g[u] {
+					if rindex[v] < rindex[u] {
+						rindex[u] = rindex[v]
+						root = false
+					}
+				}
+
+				if root {
+					// u is the local component root, so everything on the backtrack stack
+					// that has an rindex >= u's rindex is part of this component.
+					for backtrack.len > 0 && rindex[u] <= rindex[backtrack.peek()] {
+						v := backtrack.pop()
+						rindex[v] = component
+						builder.Append(v)
+						i--
+					}
+
+					rindex[u] = component
+					builder.Append(u)
+					i--
+
+					builder.FinishRow()
+					component--
+				} else {
+					// u is not a local root, so push it on the backtrack stack.
+					backtrack.push(u)
+				}
+			}
+		}
+	}
+
+	return builder.Rows
 }
 
 func writePath(path, pred []int, v int, pathLen int) []int {
