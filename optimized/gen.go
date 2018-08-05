5b5fbd9c52246055a497c4d0164e77bccdb26044
guns <self@sungpae.com>
2018-08-05 03:40:58 -0500

    Generate REP STOS memset implementations for all simple numeric types

---
+++ optimized/gen.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2018 Sung Pae <self@sungpae.com>
+// Distributed under the MIT license.
+// http://www.opensource.org/licenses/mit-license.php
+
+package optimized
+
+//go:generate ./gen_memset.rb
